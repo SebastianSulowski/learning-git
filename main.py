893cec921ccb7b8142448137b1d6e75357e1fb7f
SebastianSulowski <116625989+SebastianSulowski@users.noreply.github.com>
2022-10-25 12:45:36 +0200

    erase numbers

--- main.py
+++ main.py
@@ -11,16 +11,6 @@ print()
 print(f"kupię {counter} produkty".capitalize())
 print()
 
-print()
-numbers = []
-for numb in range(1,101):
-    numbers.append(numb)
-numbers_5 = [numb for numb in numbers if numb % 5  == 0]
-numbers_3 = [numb*numb*numb for numb in numbers if numb % 5  == 0]
-print(f"liczb od 1 do 100, które są podzielne przez 5: {numbers_5}")
-print(f"Licby od 1 do 100, które są podzielne przez 5 po podniesienieu do sześcianu: {numbers_3}")
-print()
-
 
 
 
